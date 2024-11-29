8c14f66aced94c9f5703a7dd31f27fe1b67cfde4
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2024-11-29 11:53:06 +0100

    Handle lines with several quotes

--- lua/util.lua
+++ lua/util.lua
@@ -1,82 +1,88 @@
 local M = {}
 
-local quotes = {"'", "\""} -- TODO: add backticks for some files
-local indexes_list = {}
+local function find_quote_pairs(str)
+  local pairs = {}  -- Table to store the pairs of indexes
+  local length = #str
+  local i = 1  -- Start iterating from the first character
+  local lead_quote
 
-local function get_indexes(line, substring)
-  local indexes_list = {}
-  local index = 0
-  while index do
-    index = string.find(line, substring, index + 1)
-    if index and ((line:sub(index-1, index-1) ~= '\\') or (line:sub(index-2, index-2) == '\\')) then
-      indexes_list[#indexes_list+1] = index
-    end
+  while i <= length do
+      local char = str:sub(i, i)
+      local char_is_quote = (char == '"' or char == "'")
+      local char_matches_first_quote = (lead_quote == nil or char == lead_quote)
+
+      -- Check if the character is a single or double quote
+      if char_is_quote and char_matches_first_quote then
+          -- We found a quote, now find its matching pair
+          local quote = char
+          local escaped = false
+          local pair_start = i
+
+          -- Move forward to find the matching quote
+          for j = i + 1, length do
+              local next_char = str:sub(j, j)
+
+              -- Check if it's an escaped quote
+              if next_char == '\\' then
+                  escaped = not escaped  -- Toggle escape flag
+              elseif next_char == quote and not escaped then
+                  table.insert(pairs, {pair_start, j})
+                  lead_quote = char
+                  i = j  -- Move past the matching quote
+                  break
+              else
+                  escaped = false  -- Reset escape flag
+              end
+          end
+      end
+
+      i = i + 1  -- Move to the next character in the string
   end
-  return indexes_list
+
+  return pairs
 end
 
-local function get_first_quote_info(indexes_list)
-  local start = nil
-  local char = nil
-  local table = {}
-  for key, table in pairs(indexes_list) do
-    if table[1] and ( not start or (table[1] < start)) then
-      start = table[1]
-      char = key
-    end
+local function toggle_quotes_at_pairs(str, quote_pairs)
+  -- Convert the string to a mutable array of characters
+  local chars = {}
+  for i = 1, #str do
+      chars[i] = str:sub(i, i)
   end
 
-  if not start then return nil end
+  -- Iterate over each quote pair
+  for _, pair in ipairs(quote_pairs) do
+      local start_idx = pair[1]
+      local end_idx = pair[2]
 
-  table.start = start
-  table.char = char
-  return table
-end
+      -- Determine which quote is being used at this position
+      local quote = chars[start_idx]
+      local new_quote = quote == '"' and "'" or '"'
 
-local function get_first_pair(indexes_list)
-  local pair = {}
-  local first_quote_info = get_first_quote_info(indexes_list)
+      -- Toggle the quotes (from ' to " or from " to ')
+      chars[start_idx] = new_quote
+      chars[end_idx] = new_quote
 
-  if not first_quote_info then return nil end
+      -- Remove redundant escapes and escape unescaped quotes
+      for i = start_idx + 1, end_idx - 1 do
+          -- If we find a redundant escape (e.g., \' or \"), remove the backslash
+          if chars[i] == "\\" and (chars[i + 1] == quote or chars[i + 1] == new_quote) then
+              chars[i] = ""  -- Remove the escape character
+          end
 
-  pair.char = first_quote_info.char
-  pair.start = first_quote_info.start
-  pair.stop = indexes_list[pair.char][2]
-  return pair
-end
-
-local function get_next_quote(quote)
-  -- TODO: generate this from quotes variable
-  local table = {}
-  table['"'] = "'"
-  table["'"] = '"'
-  return table[quote]
-end
-
-local function toggle_pair(line, start, stop)
-  local quote, next_quote, quoted_text, inner_text, new_inner_text, new_quoted_text, new_line
-
-  quote = line:sub(start, start)
-  next_quote = get_next_quote(quote)
-  quoted_text = line:sub(start, stop)
-  inner_text = line:sub(start + 1, stop - 1)
-  new_inner_text = string.gsub(inner_text, '\\' .. quote, quote)
-  new_inner_text = string.gsub(new_inner_text, next_quote, '\\' .. next_quote)
-  new_quoted_text = next_quote .. new_inner_text .. next_quote
-  new_line = string.gsub(line, quoted_text, new_quoted_text)
-  return new_line
-end
-
-local function toggle_quotes(line)
-  for i, quote in ipairs(quotes) do
-    indexes_list[quote] = get_indexes(line, quote)
+          -- Escape quotes inside the string if not already escaped
+          if chars[i] == new_quote and (i == 1 or chars[i - 1] ~= '\\') then
+              chars[i] = '\\' .. new_quote
+          end
+      end
   end
-  local first_pair = get_first_pair(indexes_list)
 
-  if not first_pair then return line end
+  -- Convert the characters back to a string
+  return table.concat(chars)
+end
 
-  local new_line = toggle_pair(line, first_pair.start, first_pair.stop)
-  return new_line
+
+local function toggle_quotes(str)
+  return toggle_quotes_at_pairs(str, find_quote_pairs(str))
 end
 
 M.toggle_quotes = toggle_quotes
--- test.lua
+++ test.lua
@@ -3,18 +3,56 @@
 package.loaded['lua.util'] = nil
 local util = require("lua.util")
 
-function assert_equal(expected, actual)
+local function assert_array_equal(expected, actual)
+  -- Check if the lengths are the same
+  if #expected ~= #actual then
+      error("Arrays have different lengths! Expected length: " .. #expected .. ", Actual length: " .. #actual)
+  end
+
+  -- Check if each element in the arrays is the same
+  for i = 1, #expected do
+      local expected_subarray = expected[i]
+      local actual_subarray = actual[i]
+
+      -- Check if the sub-arrays are of the same length
+      if #expected_subarray ~= #actual_subarray then
+          error("Sub-arrays at index " .. i .. " have different lengths!")
+      end
+
+      -- Compare corresponding elements of the sub-arrays
+      for j = 1, #expected_subarray do
+          if expected_subarray[j] ~= actual_subarray[j] then
+              error("Arrays are not equal at index " .. i .. ", " .. j ..
+                  "!\nExpected: " .. expected_subarray[j] .. "\nActual: " .. actual_subarray[j])
+          end
+      end
+  end
+end
+
+local function assert_string_equal(expected, actual)
   if expected ~= actual then
     error('\nexpected: ' .. expected  .. '\n' .. 'actual: ' .. actual)
   end
 end
 
-assert_equal([['']], util.toggle_quotes([[""]]))
-assert_equal([[""]], util.toggle_quotes([['']]))
-assert_equal([[]], util.toggle_quotes([[]]))
-assert_equal('"test"', util.toggle_quotes("'test'"))
-assert_equal("'test'", util.toggle_quotes('"test"'))
-assert_equal([['te\'st']], util.toggle_quotes([["te'st"]]))
-assert_equal([["te'st"]], util.toggle_quotes([['te\'st']]))
-assert_equal([['Test', 'Test2']], util.toggle_quotes([["Test", "Test2"]]))
+-- assert_array_equal({{1, 2}}, util.find_quote_pairs([['']]))
+-- assert_array_equal({{1, 2}}, util.find_quote_pairs([[""]]))
+-- assert_array_equal({{1, 4}}, util.find_quote_pairs([["''"]]))
+-- assert_array_equal({{1, 4}}, util.find_quote_pairs([['""']]))
+-- assert_array_equal({{1, 4}}, util.find_quote_pairs([['""']]))
+-- assert_array_equal({{4, 5}}, util.find_quote_pairs([[', ""]]))
+-- assert_array_equal({{1, 2}, {3, 4}}, util.find_quote_pairs([['''']]))
+
+assert_string_equal([['']], util.toggle_quotes([[""]]))
+assert_string_equal([[""]], util.toggle_quotes([['']]))
+assert_string_equal([[]], util.toggle_quotes([[]]))
+assert_string_equal([["test"]], util.toggle_quotes([['test']]))
+assert_string_equal([['test']], util.toggle_quotes([["test"]]))
+assert_string_equal([['te\'st']], util.toggle_quotes([["te'st"]]))
+assert_string_equal([["te'st"]], util.toggle_quotes([['te\'st']]))
+assert_string_equal([['', '']], util.toggle_quotes([["", ""]]))
+assert_string_equal([[', '']], util.toggle_quotes([[', ""]]))
+assert_string_equal([['', '']], util.toggle_quotes([["", '']]))
+assert_string_equal([[' "" ']], util.toggle_quotes([[" \"\" "]]))
+
 print('Tests are green')
